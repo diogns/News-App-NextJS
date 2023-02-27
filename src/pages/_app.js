85718996c076b27dd1acf24a535d2918c292dd9e
rmorales <raul.morales@entel.pe>
2023-02-27 12:56:35 -0500

    update home

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import Head from 'next/head'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <>
+      <Head>
+        <title>📰 News App</title>
+        <meta name="description" content="News app - The best app to read news" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>📰 News App</header>
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ src/pages/article/[id].js
@@ -0,0 +1,5 @@
+export default function Article() {
+    return (
+        <h1>Esto es un articulo</h1>
+    )
+}
